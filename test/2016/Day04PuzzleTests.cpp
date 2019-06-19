be215de2fe685359b94269b00b9965bfea9d558a
Mark <markdavison0+github@gmail.com>
2019-06-19 17:11:43 +1200

    Complete Day 4

--- test/2016/Day04PuzzleTests.cpp
+++ test/2016/Day04PuzzleTests.cpp
@@ -4,23 +4,20 @@
 namespace TwentySixteen {
 
 	TEST_CASE("Day 4 Part 1 Example work", "[2016][Day04]") {
-	    const std::vector<std::string> input = {};
+	    const std::vector<std::string> input = {
+			"aaaaa-bbb-z-y-x-123[abxyz]",
+			"a-b-c-d-e-f-g-h-987[abcde]",
+			"not-a-real-room-404[oarel]",
+			"totally-real-room-200[decoy]"
+		};
 
 	    Day04Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
-	}
 
-	TEST_CASE("Day 4 Part 2 Example work", "[2016][Day04]") {
-	    const std::vector<std::string> input = {};
-
-	    Day04Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("1514" == answers.first);
 	}
 
 }

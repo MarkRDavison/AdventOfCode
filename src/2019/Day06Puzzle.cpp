62dfe8fd5f08ae4c70e1a83598cce3776f1c0635
Mark <markdavison0+github@gmail.com>
2019-12-06 18:30:09 +1300

    Complete day 6

--- src/2019/Day06Puzzle.cpp
+++ src/2019/Day06Puzzle.cpp
@@ -1,10 +1,24 @@
 #include <2019/Day06Puzzle.hpp>
 #include <Core/StringExtensions.hpp>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
 
 namespace TwentyNineteen {
 	
+	struct Orbit {
+		Orbit(const std::string& _orbitee, const std::string& _orbiter) :
+			Orbitee(_orbitee),
+			Orbiter(_orbiter) {
+
+		}
+
+		const std::string& Orbitee;
+		const std::string& Orbiter;
+	};
+
 	Day06Puzzle::Day06Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2019, 6) {
+		core::PuzzleBase("Universal Orbit Map", 2019, 6) {
 
 	}
 	Day06Puzzle::~Day06Puzzle() {
@@ -21,6 +35,68 @@ namespace TwentyNineteen {
 	}
 
 	std::pair<std::string, std::string> Day06Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+
+		std::unordered_map<std::string, std::unordered_set<std::string>> orbits;
+		std::unordered_map<std::string, unsigned> memoizedOrbits;
+		std::unordered_map<std::string, std::string> whoDoIOrbit;
+		std::unordered_set<std::string> allBodies;
+		allBodies.insert("COM");
+		memoizedOrbits["COM"] = 0;
+
+		for (const auto& str : m_InputLines) {
+			const auto& pair = core::StringExtensions::splitStringByDelimeter(str, ")");
+			const auto& orbitee = pair[0];
+			const auto& orbiter = pair[1];
+
+			orbits[orbitee].insert(orbiter);
+			allBodies.insert(orbiter);
+		}
+
+		const std::string you = "YOU";
+		const std::string santa = "SAN";
+
+		std::vector<std::string> toodos = { "COM" };
+		for (unsigned i = 0; i < toodos.size(); ++i) {
+			auto& current = memoizedOrbits[toodos[i]];
+
+			for (const auto& orbiter : orbits[toodos[i]]) {
+				memoizedOrbits[orbiter] = current + 1;
+				toodos.push_back(orbiter);
+				whoDoIOrbit[orbiter] = toodos[i];
+			}
+		}
+		unsigned total = 0;
+		for (const auto& oc : memoizedOrbits) {
+			total += oc.second;
+		}
+
+		std::vector<std::string> fromYou;
+		std::unordered_set<std::string> fromYouSet;
+		std::string currentNode = you;
+		while (true) {
+			currentNode = whoDoIOrbit[currentNode];
+			fromYou.push_back(currentNode);
+			fromYouSet.insert(currentNode);
+			if (currentNode == "COM") {
+				break;
+			}
+		}
+
+		std::vector<std::string> fromSanta;
+		currentNode = santa;
+		while (true) {
+			currentNode = whoDoIOrbit[currentNode];
+			if (fromYouSet.count(currentNode) > 0) {
+				break;
+			}
+		}
+
+		auto memoizedYou = memoizedOrbits[you];
+		auto memoizedSanta = memoizedOrbits[santa];
+		auto memoizedAncestor = memoizedOrbits[currentNode];
+
+		auto answer = (memoizedYou - memoizedAncestor) + (memoizedSanta - memoizedAncestor) - 2;
+
+		return { std::to_string(total), std::to_string(answer) };
 	}
 }
--- test/2019/Day06PuzzleTests.cpp
+++ test/2019/Day06PuzzleTests.cpp
@@ -2,25 +2,31 @@
 #include <2019/Day06Puzzle.hpp>
 
 namespace TwentyNineteen {
-
-	TEST_CASE("Day 6 Part 1 Example work", "[2019][Day06]") {
-	    const std::vector<std::string> input = {};
+	
+	TEST_CASE("Day 6 Example work", "[2019][Day06]") {
+	    const std::vector<std::string> input = {
+"COM)B",
+"B)C",
+"C)D",
+"D)E",
+"E)F",
+"B)G",
+"G)H",
+"D)I",
+"E)J",
+"J)K",
+"K)L",
+"K)YOU",
+"I)SAN" };
 
 	    Day06Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
-	}
 
-	TEST_CASE("Day 6 Part 2 Example work", "[2019][Day06]") {
-	    const std::vector<std::string> input = {};
-
-	    Day06Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("54" == answers.first);
+		REQUIRE("4" == answers.second);
 	}
 
 }

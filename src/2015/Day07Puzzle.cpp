771580d68aa1343b0d8b2466c8c5577a5df9d9c6
Mark <markdavison0+github@gmail.com>
2019-12-07 06:20:10 +1300

    Complete 2015 day 7

--- src/2015/Day07Puzzle.cpp
+++ src/2015/Day07Puzzle.cpp
@@ -1,8 +1,16 @@
 #include <2015/Day07Puzzle.hpp>
 #include <Core/StringExtensions.hpp>
 
+#define TOKEN_NOT "NOT"
+#define TOKEN_AND "AND"
+#define TOKEN_OR "OR"
+#define TOKEN_LSHIFT "LSHIFT"
+#define TOKEN_RSHIFT "RSHIFT"
+#define TOKEN_ASSIGN "->"
+
 namespace TwentyFifteen {
-	
+
+
 	Day07Puzzle::Day07Puzzle() :
 		core::PuzzleBase("Some Assembly Required", 2015, 7) {
 
@@ -20,7 +28,238 @@ namespace TwentyFifteen {
 		m_InputLines = std::vector<std::string>(_inputLines);
 	}
 
+	void  Day07Puzzle::compileAssignInstruction(Computer& _computer, const Day07Puzzle::ParsedInstruction& _instr) {
+		_computer.p.emplace(_instr.output, [_instr]() -> InstructionDataType {
+			//std::cout << "Executing " << _instr.input1Int << " -> " << _instr.output << std::endl;
+			return _instr.input1Int; 
+		});
+	}
+	void Day07Puzzle::compileAndInstruction(Computer& _computer, const Day07Puzzle::ParsedInstruction& _instr) {
+		_computer.p.emplace(_instr.output, [_instr, &_computer]() -> InstructionDataType {
+			if (_instr.input1.empty()) {
+				if (_computer.memoizedValues.count(_instr.output) > 0) {
+					return _computer.memoizedValues[_instr.output];
+				}
+				//std::cout << "Executing " << _instr.input1Int << " AND " << _instr.input2 << " -> " << _instr.output << std::endl;
+				const auto& i1 = _instr.input1Int;
+				const auto& i2 = _computer.p[_instr.input2]();
+				const auto res = i1 & i2;
+				_computer.memoizedValues[_instr.output] = res;
+				return res;
+			} else {
+				if (_computer.memoizedValues.count(_instr.output) > 0) {
+					return _computer.memoizedValues[_instr.output];
+				}
+				//std::cout << "Executing " << _instr.input1 << " AND " << _instr.input2 << " -> " << _instr.output << std::endl;
+				const auto& i1 = _computer.p[_instr.input1]();
+				const auto& i2 = _computer.p[_instr.input2]();
+				const auto res = i1 & i2;
+				_computer.memoizedValues[_instr.output] = res;
+				return res;
+			}
+		});
+	}
+	void Day07Puzzle::compileOrInstruction(Computer& _computer, const Day07Puzzle::ParsedInstruction& _instr) {
+		_computer.p.emplace(_instr.output, [_instr, &_computer]() -> InstructionDataType {
+			if (_computer.memoizedValues.count(_instr.output) > 0) {
+				return _computer.memoizedValues[_instr.output];
+			}
+			//std::cout << "Executing " << _instr.input1 << " OR " << _instr.input2 << " -> " << _instr.output << std::endl;
+			const auto& i1 = _computer.p[_instr.input1]();
+			const auto& i2 = _computer.p[_instr.input2]();
+			const auto res = i1 | i2;
+			_computer.memoizedValues[_instr.output] = res;
+			return res;
+		});
+	}
+	void Day07Puzzle::compileLeftShiftInstruction(Computer& _computer, const Day07Puzzle::ParsedInstruction& _instr) {
+		_computer.p.emplace(_instr.output, [_instr, &_computer]() -> InstructionDataType {
+			if (_computer.memoizedValues.count(_instr.output) > 0) {
+				return _computer.memoizedValues[_instr.output];
+			}
+			//std::cout << "Executing " << _instr.input1 << " LSHIFT " << _instr.input2Int << " -> " << _instr.output << std::endl;
+			const auto& i1 = _computer.p[_instr.input1]();
+			const auto& i2 = _instr.input2Int;
+			const auto res = i1 << i2;
+			_computer.memoizedValues[_instr.output] = res;
+			return res;
+		});
+	}
+	void Day07Puzzle::compileRightShiftInstruction(Computer& _computer, const Day07Puzzle::ParsedInstruction& _instr) {
+		_computer.p.emplace(_instr.output, [_instr, &_computer]() -> InstructionDataType {
+			if (_computer.memoizedValues.count(_instr.output) > 0) {
+				return _computer.memoizedValues[_instr.output];
+			}
+			//std::cout << "Executing " << _instr.input1 << " RSHIFT " << _instr.input2Int << " -> " << _instr.output << std::endl;
+			const auto& i1 = _computer.p[_instr.input1]();
+			const auto& i2 = _instr.input2Int;
+			const auto res = i1 >> i2;
+			_computer.memoizedValues[_instr.output] = res;
+			return res;
+		});
+	}
+	void Day07Puzzle::compileNotInstruction(Computer& _computer, const Day07Puzzle::ParsedInstruction& _instr) {
+		_computer.p.emplace(_instr.output, [_instr, &_computer]() -> InstructionDataType {
+			if (_computer.memoizedValues.count(_instr.output) > 0) {
+				return _computer.memoizedValues[_instr.output];
+			}
+			//std::cout << "Executing NOT " << _instr.input1 << " -> " << _instr.output << std::endl;
+			const auto& i1 = _computer.p[_instr.input1]();
+			const auto res = ~i1 & 0xffff;
+			_computer.memoizedValues[_instr.output] = res;
+			return res;
+		});
+	}
+	void Day07Puzzle::compileNoOpInstruction(Computer& _computer, const Day07Puzzle::ParsedInstruction& _instr) {
+		_computer.p.emplace(_instr.output, [_instr, &_computer]() -> InstructionDataType {
+			if (_computer.memoizedValues.count(_instr.output) > 0) {
+				return _computer.memoizedValues[_instr.output];
+			}
+			//std::cout << "Executing " << _instr.input1 << " -> " << _instr.output << std::endl;
+			const auto& i1 = _computer.p[_instr.input1]();
+			const auto res = i1;
+			_computer.memoizedValues[_instr.output] = res;
+			return res;
+		});
+	}
+
 	std::pair<std::string, std::string> Day07Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+		InstructionDataType part1;
+		
+		{
+			Computer computer;
+
+			std::vector<ParsedInstruction> instructions;
+			for (const auto& l : m_InputLines) {
+				const auto& instr = parseInstruction(l);
+				switch (instr.type) {
+				case ParsedInstruction::OperatorType::ASSIGN:
+					compileAssignInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::AND:
+					compileAndInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::OR:
+					compileOrInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::LSHIFT:
+					compileLeftShiftInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::RSHIFT:
+					compileRightShiftInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::NOT:
+					compileNotInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::NOOP:
+					compileNoOpInstruction(computer, instr);
+					break;
+				}
+			}
+
+
+			auto fxn = computer.p[desiredoutput];
+			const auto res = fxn();
+			part1 = res;
+		}
+
+		if (desiredoutput == "a") {
+			Computer computer;
+
+			std::vector<ParsedInstruction> instructions;
+			for (const auto& l : m_InputLines) {
+				auto instr = parseInstruction(l);
+				if (instr.output == "b") {
+					instr.input1Int = part1;
+					instr.input1 = "";
+					instr.type = ParsedInstruction::OperatorType::ASSIGN;
+				}
+				switch (instr.type) {
+				case ParsedInstruction::OperatorType::ASSIGN:
+					compileAssignInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::AND:
+					compileAndInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::OR:
+					compileOrInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::LSHIFT:
+					compileLeftShiftInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::RSHIFT:
+					compileRightShiftInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::NOT:
+					compileNotInstruction(computer, instr);
+					break;
+				case ParsedInstruction::OperatorType::NOOP:
+					compileNoOpInstruction(computer, instr);
+					break;
+				}
+			}
+
+
+			auto fxn = computer.p[desiredoutput];
+			const auto res = fxn();
+			return { std::to_string(part1), std::to_string(res) };
+		}
+					   
+		return { std::to_string(part1), "Part 2" };
+	}
+
+	Day07Puzzle::ParsedInstruction Day07Puzzle::parseInstruction(const std::string& _line) {
+		ParsedInstruction instr{};
+		const auto& parts = core::StringExtensions::splitStringByDelimeter(_line, " ");
+		if (parts[0] == TOKEN_NOT) {
+			instr.type = ParsedInstruction::OperatorType::NOT;
+			instr.input1 = parts[1];
+			instr.output = parts[3];
+		}
+		else if (parts[1] == TOKEN_AND) {
+			instr.type = ParsedInstruction::OperatorType::AND;
+
+			if (std::isalpha(parts[0][0])) {
+				instr.input1 = parts[0];
+			} else {
+				instr.input1Int = std::stoi(parts[0]);
+			}
+			instr.input2 = parts[2];
+			instr.output = parts[4];
+		}
+		else if (parts[1] == TOKEN_OR) {
+			instr.type = ParsedInstruction::OperatorType::OR;
+			instr.input1 = parts[0];
+			instr.input2 = parts[2];
+			instr.output = parts[4];
+		}
+		else if (parts[1] == TOKEN_LSHIFT) {
+			instr.type = ParsedInstruction::OperatorType::LSHIFT;
+			instr.input1 = parts[0];
+			instr.input2Int = std::stoi(parts[2]);
+			instr.output = parts[4];
+		}
+		else if (parts[1] == TOKEN_RSHIFT) {
+			instr.type = ParsedInstruction::OperatorType::RSHIFT;
+			instr.input1 = parts[0];
+			instr.input2Int = std::stoi(parts[2]);
+			instr.output = parts[4];
+		}
+		else if (parts.size() == 3) {
+			if (std::isalpha(parts[0][0])) {
+				instr.type = ParsedInstruction::OperatorType::NOOP;
+				instr.input1 = parts[0];
+				instr.output = parts[2];
+			}
+			else {
+				instr.type = ParsedInstruction::OperatorType::ASSIGN;
+				instr.input1Int = std::stoi(parts[0]);
+				instr.output = parts[2];
+			}
+		} else {
+			instr.type = ParsedInstruction::OperatorType::INVALID;
+			return instr;
+		}
+		return instr;
 	}
 }
--- test/2015/Day07PuzzleTests.cpp
+++ test/2015/Day07PuzzleTests.cpp
@@ -3,12 +3,44 @@
 
 namespace TwentyFifteen {
 
+	TEST_CASE("Day 7 parse instructions works", "[2015][Day07]") {
+		auto instr = Day07Puzzle::parseInstruction("123 -> x");
+		REQUIRE(Day07Puzzle::ParsedInstruction::OperatorType::ASSIGN == instr.type);
+		REQUIRE(123 == instr.input1Int);
+		REQUIRE("x" == instr.output);
+		instr = Day07Puzzle::parseInstruction("x AND y -> d");
+		REQUIRE(Day07Puzzle::ParsedInstruction::OperatorType::AND == instr.type);
+		REQUIRE("x" == instr.input1);
+		REQUIRE("y" == instr.input2);
+		REQUIRE("d" == instr.output);
+		instr = Day07Puzzle::parseInstruction("x OR y -> e");
+		REQUIRE(Day07Puzzle::ParsedInstruction::OperatorType::OR == instr.type);
+		REQUIRE("x" == instr.input1);
+		REQUIRE("y" == instr.input2);
+		REQUIRE("e" == instr.output);
+		instr = Day07Puzzle::parseInstruction("x LSHIFT 2 -> f");
+		REQUIRE(Day07Puzzle::ParsedInstruction::OperatorType::LSHIFT == instr.type);
+		REQUIRE("x" == instr.input1);
+		REQUIRE(2 == instr.input2Int);
+		REQUIRE("f" == instr.output);
+		instr = Day07Puzzle::parseInstruction("y RSHIFT 2 -> g");
+		REQUIRE(Day07Puzzle::ParsedInstruction::OperatorType::RSHIFT == instr.type);
+		REQUIRE("y" == instr.input1);
+		REQUIRE(2 == instr.input2Int);
+		REQUIRE("g" == instr.output);
+		instr = Day07Puzzle::parseInstruction("NOT x -> h");
+		REQUIRE(Day07Puzzle::ParsedInstruction::OperatorType::NOT == instr.type);
+		REQUIRE("x" == instr.input1);
+		REQUIRE("h" == instr.output);
+	}
+
 	TEST_CASE("Day 7 Part 1 Example work", "[2015][Day07]") {
 	    const std::vector<std::string> input = {
 			"123 -> x",
 			"456 -> y",
 			"x AND y -> d",
 			"x OR y -> e",
+			"x -> j",
 			"x LSHIFT 2 -> f",
 			"y RSHIFT 2 -> g",
 			"NOT x -> h",
@@ -18,18 +50,17 @@ namespace TwentyFifteen {
 	    Day07Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
-	}
-
-	TEST_CASE("Day 7 Part 2 Example work", "[2015][Day07]") {
-	    const std::vector<std::string> input = {};
-
-	    Day07Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		
+		puzzle.desiredoutput = "d";
+		REQUIRE("72" == puzzle.fastSolve().first);
+		puzzle.desiredoutput = "e";
+		REQUIRE("507" == puzzle.fastSolve().first);
+		puzzle.desiredoutput = "f";
+		REQUIRE("492" == puzzle.fastSolve().first);
+		puzzle.desiredoutput = "h";
+		REQUIRE("65412" == puzzle.fastSolve().first);
+		puzzle.desiredoutput = "j";
+		REQUIRE("123" == puzzle.fastSolve().first);
 	}
 
 }

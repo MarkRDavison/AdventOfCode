f203b9d8cc86f1148d8edd4067de4e15093ad091
Mark <markdavison0+github@gmail.com>
2019-12-09 16:17:54 +1300

    Complete day 9

--- src/2015/Day09Puzzle.cpp
+++ src/2015/Day09Puzzle.cpp
@@ -1,10 +1,13 @@
 #include <2015/Day09Puzzle.hpp>
 #include <Core/StringExtensions.hpp>
+#include <unordered_set>
+#include <algorithm>
+#include <map>
 
 namespace TwentyFifteen {
 	
 	Day09Puzzle::Day09Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2015, 9) {
+		core::PuzzleBase("All in a Single Night", 2015, 9) {
 
 	}
 	Day09Puzzle::~Day09Puzzle() {
@@ -21,6 +24,42 @@ namespace TwentyFifteen {
 	}
 
 	std::pair<std::string, std::string> Day09Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+
+		std::unordered_set<std::string> cities;
+		std::map<std::string, std::map<std::string, unsigned>> distances;
+
+		for (const auto& path : m_InputLines) {
+			const auto& parts = core::StringExtensions::splitStringByDelimeter(path, " ");
+			const std::string from = parts[0];
+			const std::string to = parts[2];
+			const float weight = std::stof(parts[4]);
+
+			cities.insert(to);
+			cities.insert(from);
+			distances[from][to] = distances[to][from] = (unsigned)weight;
+		}
+		std::vector<std::string> citiesVec;
+		for (const auto & c : cities) {
+			citiesVec.push_back(c);
+
+		}
+		unsigned bestCost = std::numeric_limits<unsigned>::max();
+		unsigned worstCost = 0;
+		std::sort(citiesVec.begin(), citiesVec.end());
+		do {
+			unsigned cost = 0;
+			for (unsigned i = 1; i < citiesVec.size(); ++i) {
+				cost += distances[citiesVec[i - 1]][citiesVec[i]];
+			}
+
+			if (cost < bestCost) {
+				bestCost = cost;
+			}
+			if (cost > worstCost) {
+				worstCost = cost;
+			}
+		} while (std::next_permutation(citiesVec.data(), citiesVec.data() + citiesVec.size()));
+
+		return { std::to_string(bestCost), std::to_string(worstCost) };
 	}
 }
--- test/2015/Day09PuzzleTests.cpp
+++ test/2015/Day09PuzzleTests.cpp
@@ -4,23 +4,35 @@
 namespace TwentyFifteen {
 
 	TEST_CASE("Day 9 Part 1 Example work", "[2015][Day09]") {
-	    const std::vector<std::string> input = {};
+	    const std::vector<std::string> input = {
+			"London to Dublin = 464",
+			"London to Belfast = 518",
+			"Dublin to Belfast = 141",
+		};
 
 	    Day09Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
+
+		REQUIRE("605" == answers.first);
 	}
 
 	TEST_CASE("Day 9 Part 2 Example work", "[2015][Day09]") {
-	    const std::vector<std::string> input = {};
+		const std::vector<std::string> input = {
+			"London to Dublin = 464",
+			"London to Belfast = 518",
+			"Dublin to Belfast = 141",
+		};
 
-	    Day09Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
+		Day09Puzzle puzzle{};
+		puzzle.setVerbose(true);
+		puzzle.setInputLines(input);
 
-	    auto answers = puzzle.fastSolve();
+		auto answers = puzzle.fastSolve();
+
+		REQUIRE("982" == answers.second);
 	}
 
 }

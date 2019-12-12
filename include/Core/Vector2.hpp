0c225b2861ef380fd683e0e9604b4d6c1ec2e920
Mark <markdavison0+github@gmail.com>
2019-12-12 21:11:41 +1300

    Add Day 10 (oops forgot)

--- include/Core/Vector2.hpp
+++ include/Core/Vector2.hpp
@@ -2,6 +2,7 @@
 #define INCLUDED_ADVENT_OF_CODE_CORE_VECTOR_2_HPP_
 
 #include <cstdlib>
+#include <cmath>
 
 namespace core {
 
@@ -11,6 +12,10 @@ namespace core {
 
 		Vector2() : Vector2(0, 0) {}
 		Vector2(T _x, T _y) : x(_x), y(_y) {}
+		template <typename U>
+		Vector2(const Vector2<U>& _other) :
+			Vector2(static_cast<T>(_other.x), static_cast<T>(_other.y)) {
+		}
 
 		T x;
 		T y;
@@ -28,13 +33,13 @@ namespace core {
 	}
 
 	template <typename T>
-	bool operator<(const Vector2<T>& _lhs, const Vector2<T>& _rhs) {
-		return _lhs.y < _rhs.y && _lhs.x < _rhs.x;
+	Vector2<T> operator+(const Vector2<T>& _lhs, const Vector2<T>& _rhs) {
+		return { _lhs.x + _rhs.x, _lhs.y + _rhs.y };
 	}
 
 	template <typename T>
-	Vector2<T> operator+(const Vector2<T>& _lhs, const Vector2<T>& _rhs) {
-		return { _lhs.x + _rhs.x, _lhs.y + _rhs.y };
+	Vector2<T> operator-(const Vector2<T>& _lhs, const Vector2<T>& _rhs) {
+		return { _lhs.x - _rhs.x, _lhs.y - _rhs.y };
 	}
 
 	template <typename T>
@@ -50,9 +55,20 @@ namespace core {
 		}
 	};
 
+
+
 	using Vector2i = Vector2<int>;
 	using Vector2f = Vector2<float>;
 
+	namespace VectorMath {
+
+		static float toAngle(const core::Vector2<float>& _vec) {
+			const float Radians = std::atan2(_vec.y, _vec.x);
+			return Radians * 180.0f / static_cast<float>(3.14159265f);
+		}
+
+	}
 }
+
 
 #endif // INCLUDED_ADVENT_OF_CODE_CORE_VECTOR_2_HPP_
--- test/2019/Day10PuzzleTests.cpp
+++ test/2019/Day10PuzzleTests.cpp
@@ -3,24 +3,171 @@
 
 namespace TwentyNineteen {
 
+	TEST_CASE("Day 10 Part 1 Example 1 work", "[2019][Day10]") {
+		const std::vector<std::string> input = {
+			".#..#",
+			".....",
+			"#####",
+			"....#",
+			"...##"
+		};
+
+		Day10Puzzle puzzle{};
+		puzzle.setVerbose(true);
+		puzzle.setInputLines(input);
+
+		auto answers = puzzle.fastSolve();
+
+		REQUIRE("8" == answers.first);
+	}
+
+	TEST_CASE("Day 10 Part 1 Example 2 work", "[2019][Day10]") {
+		const std::vector<std::string> input = {
+			"......#.#.",
+			"#..#.#....",
+			"..#######.",
+			".#.#.###..",
+			".#..#.....",
+			"..#....#.#",
+			"#..#....#.",
+			".##.#..###",
+			"##...#..#.",
+			".#....####"
+		};
+
+		Day10Puzzle puzzle{};
+		puzzle.setVerbose(true);
+		puzzle.setInputLines(input);
+
+		auto answers = puzzle.fastSolve();
+
+		REQUIRE("33" == answers.first);
+	}
+
+	TEST_CASE("Day 10 Part 1 Example 3 work", "[2019][Day10]") {
+		const std::vector<std::string> input = {
+			"#.#...#.#.",
+			".###....#.",
+			".#....#...",
+			"##.#.#.#.#",
+			"....#.#.#.",
+			".##..###.#",
+			"..#...##..",
+			"..##....##",
+			"......#...",
+			".####.###."
+		};
+
+		Day10Puzzle puzzle{};
+		puzzle.setVerbose(true);
+		puzzle.setInputLines(input);
+
+		auto answers = puzzle.fastSolve();
+
+		REQUIRE("35" == answers.first);
+	}
+
+	TEST_CASE("Day 10 Part 1 Example 4 work", "[2019][Day10]") {
+		const std::vector<std::string> input = {
+			".#..#..###",
+			"####.###.#",
+			"....###.#.",
+			"..###.##.#",
+			"##.##.#.#.",
+			"....###..#",
+			"..#.#..#.#",
+			"#..#.#.###",
+			".##...##.#",
+			".....#.#.."
+		};
+
+		Day10Puzzle puzzle{};
+		puzzle.setVerbose(true);
+		puzzle.setInputLines(input);
+
+		auto answers = puzzle.fastSolve();
+
+		REQUIRE("41" == answers.first);
+	}
+
+	TEST_CASE("Day 10 Part 1 Example 5 work", "[2019][Day10]") {
+		const std::vector<std::string> input = {
+			".#..##.###...#######",
+			"##.############..##.",
+			".#.######.########.#",
+			".###.#######.####.#.",
+			"#####.##.#.##.###.##",
+			"..#####..#.#########",
+			"####################",
+			"#.####....###.#.#.##",
+			"##.#################",
+			"#####.##.###..####..",
+			"..######..##.#######",
+			"####.##.####...##..#",
+			".#####..#.######.###",
+			"##...#.##########...",
+			"#.##########.#######",
+			".####.#.###.###.#.##",
+			"....##.##.###..#####",
+			".#.#.###########.###",
+			"#.#.#.#####.####.###",
+			"###.##.####.##.#..##",
+		};
+
+		Day10Puzzle puzzle{};
+		puzzle.setVerbose(true);
+		puzzle.setInputLines(input);
+
+		auto answers = puzzle.fastSolve();
+
+		REQUIRE("210" == answers.first);
+	}
+
 	TEST_CASE("Day 10 Part 1 Example work", "[2019][Day10]") {
-	    const std::vector<std::string> input = {};
+		const std::vector<std::string> input = {
+			".#..##.###...#######",
+			"##.############..##.",
+			".#.######.########.#",
+			".###.#######.####.#.",
+			"#####.##.#.##.###.##",
+			"..#####..#.#########",
+			"####################",
+			"#.####....###.#.#.##",
+			"##.#################",
+			"#####.##.###..####..",
+			"..######..##.#######",
+			"####.##.####...##..#",
+			".#####..#.######.###",
+			"##...#.##########...",
+			"#.##########.#######",
+			".####.#.###.###.#.##",
+			"....##.##.###..#####",
+			".#.#.###########.###",
+			"#.#.#.#####.####.###",
+			"###.##.####.##.#..##",
+		};
 
 	    Day10Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
-	    auto answers = puzzle.fastSolve();
+		auto answers = puzzle.fastSolve();
+
+		REQUIRE("802" == answers.second);
 	}
-
-	TEST_CASE("Day 10 Part 2 Example work", "[2019][Day10]") {
-	    const std::vector<std::string> input = {};
-
-	    Day10Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+	TEST_CASE("Day 10 Part 2 Example 2 work", "[2019][Day10]") {
+		const std::vector<std::string> input = {
+			".#....#####...#..",
+			"##...##.#####..##",
+			"##...#...#.#####.",
+			"..#.....#...###..",
+			"..#.#.....#....##"
+		};
+		
+		Day10Puzzle puzzle{};
+		puzzle.setVerbose(true);
+		puzzle.setInputLines(input);
+		
+		auto answers = puzzle.fastSolve();
 	}
-
 }

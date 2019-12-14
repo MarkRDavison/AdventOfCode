7a1c2f08d127d1974667d1e17de4af886bdcac1e
Mark <markdavison0+github@gmail.com>
2019-12-14 15:15:37 +1300

    Add day 13

--- test/2019/Day13PuzzleTests.cpp
+++ test/2019/Day13PuzzleTests.cpp
@@ -3,24 +3,4 @@
 
 namespace TwentyNineteen {
 
-	TEST_CASE("Day 13 Part 1 Example work", "[2019][Day13]") {
-	    const std::vector<std::string> input = {};
-
-	    Day13Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
-	}
-
-	TEST_CASE("Day 13 Part 2 Example work", "[2019][Day13]") {
-	    const std::vector<std::string> input = {};
-
-	    Day13Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
-	}
-
 }

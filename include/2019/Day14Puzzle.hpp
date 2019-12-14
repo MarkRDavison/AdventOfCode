5b46ec3d73aa6223f4070df3d52a6591052e0e07
Mark <markdavison0+github@gmail.com>
2019-12-15 00:28:15 +1300

    Add day 14

--- include/2019/Day14Puzzle.hpp
+++ include/2019/Day14Puzzle.hpp
@@ -2,8 +2,21 @@
 #define INCLUDED_ADVENT_OF_CODE_2019_DAY_14_PUZZLE_HPP_
 
 #include <Core/PuzzleBase.hpp>
+#include <unordered_map>
 
 namespace TwentyNineteen {
+
+	using ReactionId = std::string;
+	using ReactionOutput = long long;
+	struct Ingrediant {
+		ReactionId name;
+		ReactionOutput amount;
+
+	};
+	struct Reaction {
+		Ingrediant output;
+		std::vector<Ingrediant> input;
+	};
 	
 	class Day14Puzzle : public core::PuzzleBase {
 	public:
@@ -13,7 +26,12 @@ namespace TwentyNineteen {
 		void initialise(const core::InitialisationInfo& _initialisationInfo) override;
 		void setInputLines(const std::vector<std::string>& _inputLines);
 		std::pair<std::string, std::string> fastSolve() override;
-
+		ReactionOutput howMuchOre(
+			ReactionId _chemical,
+			ReactionOutput _amount,
+			std::unordered_map<ReactionId, ReactionOutput>& _excess,
+			std::unordered_map<ReactionId, Reaction>& _referenceData,
+			unsigned _depth);
 	private:
 		std::vector<std::string> m_InputLines;
 	};

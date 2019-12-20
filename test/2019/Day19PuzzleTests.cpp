10d0965393f82f83823be17030a5a2143d260b44
Mark <markdavison0+github@gmail.com>
2019-12-20 15:20:24 +1300

    Complete day 19

--- test/2019/Day19PuzzleTests.cpp
+++ test/2019/Day19PuzzleTests.cpp
@@ -3,24 +3,4 @@
 
 namespace TwentyNineteen {
 
-	TEST_CASE("Day 19 Part 1 Example work", "[2019][Day19]") {
-	    const std::vector<std::string> input = {};
-
-	    Day19Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
-	}
-
-	TEST_CASE("Day 19 Part 2 Example work", "[2019][Day19]") {
-	    const std::vector<std::string> input = {};
-
-	    Day19Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
-	}
-
 }

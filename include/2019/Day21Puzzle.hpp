0010a371fc644f9be505e3830ebc55cd2e88c899
Mark <markdavison0+github@gmail.com>
2019-12-21 19:00:46 +1300

    Complete day 21

--- include/2019/Day21Puzzle.hpp
+++ include/2019/Day21Puzzle.hpp
@@ -2,6 +2,7 @@
 #define INCLUDED_ADVENT_OF_CODE_2019_DAY_21_PUZZLE_HPP_
 
 #include <Core/PuzzleBase.hpp>
+#include <2019/IntcodeMachine.hpp>
 
 namespace TwentyNineteen {
 	
@@ -13,7 +14,7 @@ namespace TwentyNineteen {
 		void initialise(const core::InitialisationInfo& _initialisationInfo) override;
 		void setInputLines(const std::vector<std::string>& _inputLines);
 		std::pair<std::string, std::string> fastSolve() override;
-
+		IntcodeValue getResultOrDumpError(const std::vector<std::string>& _springInstructions, const std::string& _mode);
 	private:
 		std::vector<std::string> m_InputLines;
 	};

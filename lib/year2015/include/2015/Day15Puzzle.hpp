5bc64446266f144418fd1704f34d98bd92782f03
Mark <Mark@DESKTOP-V1BKCDT>
2020-07-24 11:54:52 +1200

    2015 Day 15 - brute force, not nice :(

--- lib/year2015/include/2015/Day15Puzzle.hpp
+++ lib/year2015/include/2015/Day15Puzzle.hpp
@@ -5,6 +5,22 @@
 
 namespace TwentyFifteen {
 	
+	enum class IngredientProperty {
+		Capacity,
+		Durability,
+		Flavour,
+		Texture,
+		Calories
+	};
+	struct Ingredient {
+		std::string name;
+		int capacity;
+		int durability;
+		int flavour;
+		int texture;
+		int calories;
+	};
+
 	class Day15Puzzle : public core::PuzzleBase {
 	public:
 		Day15Puzzle();
@@ -13,6 +29,9 @@ namespace TwentyFifteen {
 		void initialise(const core::InitialisationInfo& _initialisationInfo) override;
 		void setInputLines(const std::vector<std::string>& _inputLines);
 		std::pair<std::string, std::string> fastSolve() override;
+
+		static std::vector<Ingredient> parseInput(const std::vector<std::string>& _inputLines);
+
 
 	private:
 		std::vector<std::string> m_InputLines;

701dc29facd27dd723688785e16595d926c63e9d
Mark <Mark@DESKTOP-V1BKCDT>
2020-07-24 17:49:53 +1200

    2015 day 19

--- lib/year2015/include/2015/Day19Puzzle.hpp
+++ lib/year2015/include/2015/Day19Puzzle.hpp
@@ -2,9 +2,12 @@
 #define INCLUDED_ADVENT_OF_CODE_2015_DAY_19_PUZZLE_HPP_
 
 #include <Core/PuzzleBase.hpp>
+#include <unordered_map>
 
 namespace TwentyFifteen {
-	
+
+	using Replacements = std::unordered_map<std::string, std::vector<std::string>>;
+
 	class Day19Puzzle : public core::PuzzleBase {
 	public:
 		Day19Puzzle();
@@ -14,6 +17,11 @@ namespace TwentyFifteen {
 		void setInputLines(const std::vector<std::string>& _inputLines);
 		std::pair<std::string, std::string> fastSolve() override;
 
+		static std::pair<Replacements, std::string> parseInput(const std::vector<std::string>& _inputLines);
+
+		static int getDistinctMolecues(const Replacements& _replacements, const std::string& _startingMolecule);
+		static int stepsToFabricate(const Replacements& _replacements, const std::string& _target);
+		static std::vector<std::string> splitAtCapitalLetter(const std::string& _string);
 	private:
 		std::vector<std::string> m_InputLines;
 	};
--- test/2015/Day19PuzzleTests.cpp
+++ test/2015/Day19PuzzleTests.cpp
@@ -3,24 +3,61 @@
 
 namespace TwentyFifteen {
 
-	TEST_CASE("2015 Day 19 Part 1 Example work", "[2015][Day19]") {
-	    const std::vector<std::string> input = {};
-
-	    Day19Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+	TEST_CASE("2015 Day 19 capital split works", "[2015][Day19]") {
+		const auto& res = Day19Puzzle::splitAtCapitalLetter("AaaaBbbCDEeeeFFFGhHj");
+		REQUIRE(10 == res.size());
+		REQUIRE("Aaaa" == res[0]);
+		REQUIRE("Bbb" == res[1]);
+		REQUIRE("C" == res[2]);
+		REQUIRE("D" == res[3]);
+		REQUIRE("Eeee" == res[4]);
+		REQUIRE("F" == res[5]);
+		REQUIRE("F" == res[6]);
+		REQUIRE("F" == res[7]);
+		REQUIRE("Gh" == res[8]);
+		REQUIRE("Hj" == res[9]);
 	}
 
-	TEST_CASE("2015 Day 19 Part 2 Example work", "[2015][Day19]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("2015 Day 19 Parses input correctly", "[2015][Day19]") {
+		const std::vector<std::string> input = {
+"H => HO",
+"H => OH",
+"O => HH",
+"",
+"HOH"
+		};
 
-	    Day19Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
+		const auto& parsed = Day19Puzzle::parseInput(input);
 
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("HOH" == parsed.second);
+		REQUIRE(2 == parsed.first.size());
+		REQUIRE(2 == parsed.first.at("H").size());
+		REQUIRE("HO" == parsed.first.at("H")[0]);
+		REQUIRE("OH" == parsed.first.at("H")[1]);
+		REQUIRE(1 == parsed.first.at("O").size());
+		REQUIRE("HH" == parsed.first.at("O")[0]);
+
+		SECTION("Generating replacement instructions works correctly") {
+			REQUIRE(4 == Day19Puzzle::getDistinctMolecues(parsed.first, parsed.second));
+			REQUIRE(7 == Day19Puzzle::getDistinctMolecues(parsed.first, "HOHOHO"));
+		}
+	}
+
+	TEST_CASE("2015 Day 19 getting steps to generate examples work", "[2015][Day19]") {
+		const std::vector<std::string> input = {
+"e => H",
+"e => O",
+"H => HO",
+"H => OH",
+"O => HH",
+"",
+"HOH"
+		};
+
+		const auto& parsed = Day19Puzzle::parseInput(input);
+
+		REQUIRE(3 == Day19Puzzle::stepsToFabricate(parsed.first, "HOH"));
+		REQUIRE(6 == Day19Puzzle::stepsToFabricate(parsed.first, "HOHOHO"));
 	}
 
 }

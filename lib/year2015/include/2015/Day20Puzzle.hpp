5851d380c343db5b6383bbe4d1687634959fb2e3
Mark <Mark@DESKTOP-V1BKCDT>
2020-07-24 18:52:19 +1200

    2015 day 20

--- lib/year2015/include/2015/Day20Puzzle.hpp
+++ lib/year2015/include/2015/Day20Puzzle.hpp
@@ -4,7 +4,7 @@
 #include <Core/PuzzleBase.hpp>
 
 namespace TwentyFifteen {
-	
+	using BigNumberThing = long;
 	class Day20Puzzle : public core::PuzzleBase {
 	public:
 		Day20Puzzle();
@@ -13,6 +13,11 @@ namespace TwentyFifteen {
 		void initialise(const core::InitialisationInfo& _initialisationInfo) override;
 		void setInputLines(const std::vector<std::string>& _inputLines);
 		std::pair<std::string, std::string> fastSolve() override;
+
+		static BigNumberThing getNumberOfPresentsForHouseNumber(BigNumberThing _houseNumber);
+		static BigNumberThing getNumberOfPresentsForHouseNumberPart2(BigNumberThing _houseNumber);
+		static BigNumberThing getPart1(BigNumberThing _input);
+		static BigNumberThing getPart2(BigNumberThing _input);
 
 	private:
 		std::vector<std::string> m_InputLines;

7cc2f64420b6c1da18f88cf1b7d08cb3c8f82978
Mark <Mark@DESKTOP-V1BKCDT>
2020-07-25 23:59:18 +1200

    2015 Day 23.  Tried way too hard to optimise early.  Just simulating it works fine

--- test/2015/Day23PuzzleTests.cpp
+++ test/2015/Day23PuzzleTests.cpp
@@ -3,24 +3,4 @@
 
 namespace TwentyFifteen {
 
-	TEST_CASE("2015 Day 23 Part 1 Example work", "[2015][Day23]") {
-	    const std::vector<std::string> input = {};
-
-	    Day23Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
-	}
-
-	TEST_CASE("2015 Day 23 Part 2 Example work", "[2015][Day23]") {
-	    const std::vector<std::string> input = {};
-
-	    Day23Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
-	}
-
 }

0e9a93dc66d5e1cc3cff6d3da17d0796927a3165
Mark Davison <markdavison0+github@gmail.com>
2020-12-05 22:25:03 +1300

    day 5

--- lib/year2020/include/2020/Day05Puzzle.hpp
+++ lib/year2020/include/2020/Day05Puzzle.hpp
@@ -14,6 +14,14 @@ namespace TwentyTwenty {
 		void setInputLines(const std::vector<std::string>& _inputLines);
 		std::pair<std::string, std::string> fastSolve() override;
 
+		struct SeatInfo {
+			int row;
+			int column;
+			int seatId;
+		};
+
+		static SeatInfo getSeatInfo(const std::string& _input);
+
 	private:
 		std::vector<std::string> m_InputLines;
 	};
--- test/2020/Day05PuzzleTests.cpp
+++ test/2020/Day05PuzzleTests.cpp
@@ -3,24 +3,29 @@
 
 namespace TwentyTwenty {
 
-	TEST_CASE("Day 5 Part 1 Example work", "[2020][Day05]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("Day 5 getSeatInfo works", "[2020][Day05]") {
+	    
+		const auto indexes = Day05Puzzle::getSeatInfo("FBFBBFFRLR");
 
-	    Day05Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE(44 == indexes.row);
+		REQUIRE(5 == indexes.column);
+		REQUIRE(357 == indexes.seatId);
 	}
 
-	TEST_CASE("Day 5 Part 2 Example work", "[2020][Day05]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("Day 5 Part 1 Example work", "[2020][Day05]") {
+	    const std::vector<std::string> input = {
+			"BFFFBBFRRR",
+			"FFFBBBFRRR",
+			"BBFFBBFRLL"
+		};
 
 	    Day05Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
+
+		REQUIRE("820" == answers.first);
 	}
 
 }

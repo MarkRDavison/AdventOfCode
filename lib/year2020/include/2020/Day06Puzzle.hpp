9f7a3b619253f98db8b39ce4f25e0ec15c30c680
Mark Davison <markdavison0+github@gmail.com>
2020-12-06 18:19:52 +1300

    2020 day 6

--- lib/year2020/include/2020/Day06Puzzle.hpp
+++ lib/year2020/include/2020/Day06Puzzle.hpp
@@ -1,6 +1,7 @@
 #ifndef INCLUDED_ADVENT_OF_CODE_2020_DAY_06_PUZZLE_HPP_
 #define INCLUDED_ADVENT_OF_CODE_2020_DAY_06_PUZZLE_HPP_
 
+#include <set>
 #include <Core/PuzzleBase.hpp>
 
 namespace TwentyTwenty {
@@ -13,6 +14,12 @@ namespace TwentyTwenty {
 		void initialise(const core::InitialisationInfo& _initialisationInfo) override;
 		void setInputLines(const std::vector<std::string>& _inputLines);
 		std::pair<std::string, std::string> fastSolve() override;
+
+		using Questions = std::vector<std::set<char>>;
+
+		static std::vector<Questions> parseInput(const std::vector<std::string>& _input);
+		static std::string doPart1(const std::vector<Questions>& _parsedInput);
+		static std::string doPart2(const std::vector<Questions>& _parsedInput);
 
 	private:
 		std::vector<std::string> m_InputLines;
--- test/2020/Day06PuzzleTests.cpp
+++ test/2020/Day06PuzzleTests.cpp
@@ -4,23 +4,33 @@
 namespace TwentyTwenty {
 
 	TEST_CASE("Day 6 Part 1 Example work", "[2020][Day06]") {
-	    const std::vector<std::string> input = {};
+	    const std::vector<std::string> input = {
+"abc",
+"",
+"a",
+"b",
+"c",
+"",
+"ab",
+"ac",
+"",
+"a",
+"a",
+"a",
+"a",
+"",
+"b",	
+		};
 
 	    Day06Puzzle puzzle{};
 	    puzzle.setVerbose(true);
 	    puzzle.setInputLines(input);
 
 	    auto answers = puzzle.fastSolve();
+
+		REQUIRE("11" == answers.first);
+		REQUIRE("6" == answers.second);
 	}
 
-	TEST_CASE("Day 6 Part 2 Example work", "[2020][Day06]") {
-	    const std::vector<std::string> input = {};
-
-	    Day06Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
-	}
 
 }

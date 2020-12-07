6cd9dc4dddb774ef40e0aa143474c5ee6a5f232d
Mark Davison <markdavison0+github@gmail.com>
2020-12-07 18:38:32 +1300

    day 7

--- lib/year2020/src/Day07Puzzle.cpp
+++ lib/year2020/src/Day07Puzzle.cpp
@@ -1,14 +1,34 @@
 #include <2020/Day07Puzzle.hpp>
 #include <zeno/Utility/StringExtensions.hpp>
 
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <functional>
+
+#include <cassert>
+#include <set>
+
 namespace TwentyTwenty {
-	
+
+	// TODO: Move this to utility framework
+	std::vector<std::string> splitStringByLines3(const std::string& _parameter) {
+		std::vector<std::string> result;
+		std::stringstream stringStream(_parameter);
+		std::string line;
+		while (std::getline(stringStream, line)) {
+			result.push_back(line);
+		}
+
+		return result;
+	}
 	Day07Puzzle::Day07Puzzle() :
-		core::PuzzleBase("Untitled Puzzle", 2020, 7) {
+		core::PuzzleBase("", 2020, 7) {
 	}
 
 
 	void Day07Puzzle::initialise(const core::InitialisationInfo& _initialisationInfo) {
+		// setInputLines(splitStringByLines2(ze::StringExtensions::loadFileToString(_initialisationInfo.parameters[0])));
 		setInputLines(ze::StringExtensions::splitStringByDelimeter(ze::StringExtensions::loadFileToString(_initialisationInfo.parameters[0]), "\n"));
 	}
 
@@ -17,6 +37,93 @@ namespace TwentyTwenty {
 	}
 
 	std::pair<std::string, std::string> Day07Puzzle::fastSolve() {
-		return { "Part 1", "Part 2" };
+
+		const auto& parsed = Day07Puzzle::parseInput(m_InputLines);
+
+		const auto& part1 = Day07Puzzle::doPart1(parsed);
+		const auto& part2 = Day07Puzzle::doPart2(parsed);
+
+		return { part1, part2 };
+	}
+
+	std::string Day07Puzzle::doPart1(const std::vector<Day7Struct>& _input) {
+
+		const std::string start = "shiny gold";
+
+		std::map<std::string, std::set<std::string>> info;
+
+		for (const auto& s : _input) {
+			for (const auto& [k, v] : s.contained) {
+				info[k].insert(s.parent);
+			}
+		}
+
+		std::set<std::string> uniqueHeldBy;
+
+		std::function<void(const std::string&)> recurser =
+			[&recurser, &info, &uniqueHeldBy](const std::string& _current) -> void {
+			uniqueHeldBy.insert(_current);
+			if (info.count(_current) != 0) {
+				for (const auto& kv : info[_current]) {
+					recurser(kv);
+				}
+			}
+		};
+
+		recurser(start);
+
+		return std::to_string(uniqueHeldBy.size() - 1);
+	}
+	std::string Day07Puzzle::doPart2(const std::vector<Day7Struct>& _input) {
+
+		const std::string start = "shiny gold";
+
+		std::map<std::string, std::map<std::string, int>> info;
+		for (const auto& s : _input) {
+			for (const auto& [k, v] : s.contained) {
+				info[s.parent][k] += v;
+			}
+		}
+
+		std::function<int(const std::string&)> recurser = [&recurser , &info](const std::string& _current) -> int {
+			int amount = 1;
+			for (const auto& child : info[_current]) {
+				amount += child.second * recurser(child.first);
+			}
+
+			return amount;
+		};
+
+		const int bags = recurser(start) - 1;
+
+		return std::to_string(bags);
+	}
+
+	std::vector<Day7Struct> Day07Puzzle::parseInput(const std::vector<std::string>& _inputLines) {
+		std::vector<Day7Struct> parsed;
+
+		for (const auto& input : _inputLines) {
+
+			const auto& parts = ze::StringExtensions::splitStringByDelimeter(input, " ");
+
+			assert(parts[2] == "bags");
+			assert(parts[3] == "contain");
+
+			auto& next = parsed.emplace_back();
+			next.parent = parts[0] + " " + parts[1];
+
+			for (unsigned i = 4; i < parts.size(); i += 4) {
+				const std::string child = parts[i+1] + " " + parts[i+2];
+				const auto amountString = parts[i];
+				if (amountString == "no") {
+					break;
+				}
+				const int amount = std::stoi(amountString);
+
+				next.contained[child] = amount;
+			}
+		}
+
+		return parsed;
 	}
 }
--- test/2020/Day07PuzzleTests.cpp
+++ test/2020/Day07PuzzleTests.cpp
@@ -3,24 +3,26 @@
 
 namespace TwentyTwenty {
 
-	TEST_CASE("Day 7 Part 1 Example work", "[2020][Day07]") {
-	    const std::vector<std::string> input = {};
+	TEST_CASE("Day 7 Examples work", "[2020][Day07]") {
+	    const std::vector<std::string> input = {
+"light red bags contain 1 bright white bag, 2 muted yellow bags.",
+"dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+"bright white bags contain 1 shiny gold bag.",
+"muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+"shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+"dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+"vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+"faded blue bags contain no other bags.",
+"dotted black bags contain no other bags."
+		};
 
-	    Day07Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
+		const auto& parsed = Day07Puzzle::parseInput(input);
 
-	    auto answers = puzzle.fastSolve();
-	}
+		const auto& part1 = Day07Puzzle::doPart1(parsed);
+		const auto& part2 = Day07Puzzle::doPart2(parsed);
 
-	TEST_CASE("Day 7 Part 2 Example work", "[2020][Day07]") {
-	    const std::vector<std::string> input = {};
-
-	    Day07Puzzle puzzle{};
-	    puzzle.setVerbose(true);
-	    puzzle.setInputLines(input);
-
-	    auto answers = puzzle.fastSolve();
+		REQUIRE("4" == part1);
+		REQUIRE("32" == part2);
 	}
 
 }
